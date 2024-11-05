7eb217402f872b6db2bd5d7a3320048e8a01dca5
ARPIT MATHUR <78298300+arpit20328@users.noreply.github.com>
2024-11-05 15:49:48 +0530

    Update main_script.sh

--- main_script.sh
+++ main_script.sh
@@ -1,64 +1,28 @@
 #!/bin/bash
-
 mkdir  genome_annotation_by_arpit
-
 cd genome_annotation_by_arpit
-
-
 #Make your tsv file, free of special characters and spaces via following command
 sed 's/[^a-zA-Z0-9\t]//g' file.tsv > temp.tsv && mv temp.tsv file.tsv
-
-
 awk '{print $1}' file.tsv  | while read taxon_id; do datasets summary genome taxon $taxon_id --reference > taxon_$taxon_id.txt; done
-
-
-
 #Above command will give output files like taxon_573.txt  etc which will contain genome summary for that taxon 
-
 #some output files will have 0 byte size 
 #some output files will have 19 byte size
-
-
-# Define the destination directory
-destination="/home/arpit20328/bracken_genome_size_estimations/files_having_zero_or_19_byte_size"
-
-# Create the destination directory if it doesn't exist
-mkdir -p "$destination"
-
+mkdir -p files_having_zero_or_19_byte_size
 # Find and copy files that have sizes of either 0 or 19 bytes
-find . -type f -name 'taxon_*.txt' \( -size 0c -o -size 19c \) -exec mv {} "$destination" \;
-
-echo "Files with size 0 or 19 bytes have been copied to $destination"
-
-
-
-cd "$destination"
-
+find . -type f -name 'taxon_*.txt' \( -size 0c -o -size 19c \) -exec mv {} files_having_zero_or_19_byte_size/ \;
+cd files_having_zero_or_19_byte_size
 ls taxon_*.txt | sed -e 's/taxon_//' -e 's/.txt//' > extracted_numbers.tsv
-
 #Make your tsv file, free of special characters and spaces via following command
 sed 's/[^a-zA-Z0-9\t]//g' extracted_numbers.tsv > temp.tsv && mv temp.tsv extracted_numbers.tsv
- 
 awk '{print $1}' extracted_numbers.tsv  | while read taxon_id; do datasets summary genome taxon $taxon_id > taxon_$taxon_id.txt; done
-
-mkdir  "$destination/files_size_less_than_3k_bytes
-
-find $PWD -type f -name "*.txt" -size -3k -exec mv {} files_size_less_than_3k_byte/ \;
-
-cd   $destination/files_size_less_than_3k_bytes
-
+mkdir  files_size_less_than_3k_bytes
+find $PWD -type f -name "*.txt" -size -3k -exec mv {} $PWD/files_size_less_than_3k_bytes/ \;
+cd files_size_less_than_3k_bytes
 #another round to make these sizes are not extractable 
-
 ls taxon_*.txt | sed -e 's/taxon_//' -e 's/.txt//' > extracted_numbers.tsv
-
 #Make your tsv file, free of special characters and spaces via following command
 sed 's/[^a-zA-Z0-9\t]//g' extracted_numbers.tsv > temp.tsv && mv temp.tsv extracted_numbers.tsv
-
-
 awk '{print $1}' extracted_numbers.tsv  | while read taxon_id; do datasets summary genome taxon $taxon_id > taxon_$taxon_id.txt; done
-
-
-
 find $PWD -type f -name "*.txt" -size +3k -exec mv {} /home/arpit20328/bracken_genome_size_estimations/files_having_zero_or_19_byte_size/  \;
 
 
